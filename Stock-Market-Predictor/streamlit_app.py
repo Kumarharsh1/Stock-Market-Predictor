5bc61fe3b4c6d0f2b5d9edf0a694476325a90c0b
KUMAR HARSH <93419195+Kumarharsh1@users.noreply.github.com>
2025-07-31 21:18:29 +0530

    streamlit_app.py

--- Stock-Market-Predictor/streamlit_app.py
+++ Stock-Market-Predictor/streamlit_app.py
@@ -7,8 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1DC4FQha6h6xj1Czm7x0Jpa_UKZbZArjS
 """
 
-from google.colab import files
-uploaded = files.upload()
+
 
 
 # Load and rename columns if necessary
